6bdbb72951b7e67fd377269c2c02eca4c473d5b2
CoderDk <dhruvkhuranaa2006@gmail.com>
2026-03-07 12:17:07 +0530

    Implement functions to add and retrieve book reviews

---
+++ review.py
@@ -0,0 +1,26 @@
+from database import connect_db
+
+def add_review(book_id,rating,review):
+
+    conn = connect_db()
+    cursor = conn.cursor()
+
+    cursor.execute(
+    "INSERT INTO reviews(book_id,rating,review) VALUES(?,?,?)",
+    (book_id,rating,review)
+    )
+
+    conn.commit()
+
+
+def get_reviews(book_id):
+
+    conn = connect_db()
+    cursor = conn.cursor()
+
+    cursor.execute(
+    "SELECT rating,review FROM reviews WHERE book_id=?",
+    (book_id,)
+    )
+
+    return cursor.fetchall()
