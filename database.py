988d55ecea7845616c9b3eb7aee5288e3c94a1a2
CoderDk <dhruvkhuranaa2006@gmail.com>
2026-03-07 15:02:10 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,25 +1,25 @@
 import sqlite3
 
 def connect_db():
-    conn = sqlite3.connect("library.db")
+    conn = sqlite3.connect("library.db",check_same_thread=False)
     return conn
-
 
 def create_tables():
 
     conn = connect_db()
-    cursor = conn.cursor()
+    c = conn.cursor()
 
-    cursor.execute("""
+    c.execute("""
     CREATE TABLE IF NOT EXISTS users(
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     name TEXT,
     email TEXT,
-    password TEXT
+    password TEXT,
+    role TEXT
     )
     """)
 
-    cursor.execute("""
+    c.execute("""
     CREATE TABLE IF NOT EXISTS books(
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     title TEXT,
@@ -28,22 +28,14 @@ def create_tables():
     )
     """)
 
-    cursor.execute("""
+    c.execute("""
     CREATE TABLE IF NOT EXISTS borrow(
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     user_id INTEGER,
-    book_id INTEGER
-    )
-    """)
-
-    cursor.execute("""
-    CREATE TABLE IF NOT EXISTS reviews(
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
     book_id INTEGER,
-    rating INTEGER,
-    review TEXT
+    issue_date TEXT,
+    return_date TEXT
     )
     """)
 
     conn.commit()
-    conn.close()
