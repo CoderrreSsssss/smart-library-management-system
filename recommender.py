19a3892aec9e793522f0c14933c004a10e950780
CoderDk <dhruvkhuranaa2006@gmail.com>
2026-03-07 12:17:52 +0530

    Refactor recommend function to use cosine similarity

--- recommender.py
+++ recommender.py
@@ -1,14 +1,26 @@
 import pandas as pd
+from sklearn.metrics.pairwise import cosine_similarity
 
-def recommend(book_title, books):
+def recommend():
 
-    df = pd.DataFrame(books)
+    data = {
+    "book":["Python","AI Basics","Machine Learning","Deep Learning","SQL Guide"],
+    "python":[1,0,0,0,0],
+    "ai":[0,1,1,1,0],
+    "data":[0,1,1,1,1]
+    }
 
-    if book_title not in df["title"].values:
-        return []
+    df = pd.DataFrame(data)
 
-    genre = df[df["title"] == book_title]["genre"].values[0]
+    similarity = cosine_similarity(df.iloc[:,1:])
 
-    rec = df[df["genre"] == genre]["title"].tolist()
+    scores = list(enumerate(similarity[0]))
 
-    return [b for b in rec if b != book_title][:5]
+    scores = sorted(scores,key=lambda x:x[1],reverse=True)
+
+    rec = []
+
+    for i in scores[1:4]:
+        rec.append(df.iloc[i[0]].book)
+
+    return rec
